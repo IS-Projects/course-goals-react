74f2cb87887889addd142d9ef7238b3ccdb19e15
Tejada-Joyce <joyce_105_3@hotmail.com>
2022-01-14 22:18:13 -0700

    made input field be empty once goal got added

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -58,6 +58,7 @@ const CourseInput = (props) => {
       return;
     }
     props.onAddGoal(enteredValue);
+    setEnteredValue("");
   };
 
   return (
@@ -68,7 +69,11 @@ const CourseInput = (props) => {
         className={`${styles["form-control"]} ${!isValid && styles.invalid}`}
       >
         <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        <input
+          type="text"
+          value={enteredValue}
+          onChange={goalInputChangeHandler}
+        />
       </div>
       {/* </FormControl> */}
       <Button type="submit">Add Goal</Button>
